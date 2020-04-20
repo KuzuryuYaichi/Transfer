0f82f8782c26e6c4a267b5c3a077a2ad020f4d6b
KuzuryuYaichi <2731820096@qq.com>
2020-04-20 15:42:34 +0800

    del

--- Terminal_BOOT/CRC.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CRC_TERMINAL_H_
-#define _CRC_TERMINAL_H_
-
-static __u16 crc_ccitt(__u16 crc, __u8 c);
-__u16 CRC16_CCITT_XMODEM(__u8 * Data ,__u32 Len);
-
-#endif
--- Terminal_BOOT/can.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CAN_TERMINAL_H_
-#define _CAN_TERMINAL_H_
-
-int CAN_init(void);
-
-#endif
